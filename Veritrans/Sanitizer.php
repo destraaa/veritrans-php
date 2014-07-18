f60595bd6ae6bc5a7fa600f4821ecb41bafef640
Destra Bintang Perkasa <destra.bintang.perkasa@gmail.com>
2014-07-18 13:48:13 +0700

    Fix Sanitize.php error for PHP version below 5.4

--- Veritrans/Sanitizer.php
+++ Veritrans/Sanitizer.php
@@ -23,28 +23,35 @@ class Veritrans_Sanitizer {
   private static function fieldItemDetails(&$items)
   {
     foreach ($items as &$item) {
-      $item['id'] = (new self)
-          ->maxLength(50)
-          ->apply($item['id']);
-      $item['name'] = (new self)
-          ->maxLength(50)
-          ->apply($item['name']);
+	  $temp = (new self);
+	  $temp = $temp -> maxLength(50);
+	  $temp = $temp -> apply($item['id']);
+	  $item['id'] = $temp;
+	  
+	  $temp = (new self);
+	  $temp = $temp -> maxLength(50);
+	  $temp = $temp -> apply($item['name']);
+	  $item['name'] = $temp;
     }
   }
 
   private static function fieldCustomerDetails(&$field)
   {
-    $field['first_name'] = (new self)
-        ->maxLength(20)
-        ->apply($field['first_name']);
+	$temp = (new self);
+	$temp = $temp -> maxLength(20);
+	$temp = $temp -> apply($field['first_name']);;
+    $field['first_name'] = $temp;
+	
     if (array_key_exists('last_name', $field)) {
-      $field['last_name'] = (new self)
-          ->maxLength(20)
-          ->apply($field['last_name']);
+		$temp = (new self);
+		$temp = $temp -> maxLength(20);
+		$temp = $temp -> apply($field['last_name']);;
+		$field['last_name'] = $temp;
     }
-    $field['email'] = (new self)
-        ->maxLength(45)
-        ->apply($field['email']);
+	$temp = (new self);
+	$temp = $temp -> maxLength(45);
+	$temp = $temp -> apply($field['email']);;
+    $field['email'] = $temp;
 
     static::fieldPhone($field['phone']);
 
@@ -70,17 +77,19 @@ class Veritrans_Sanitizer {
 
     foreach ($fields as $key => $value) {
       if (array_key_exists($key, $field)) {
-        $field[$key] = (new self)
-            ->maxLength($value)
-            ->apply($field[$key]);
+		$temp = (new self);
+		$temp = $temp -> maxLength($value);
+		$temp = $temp -> apply($field[$key]);;
+		$field[$key] = $temp;
       }
     }
 
     if (array_key_exists('postal_code', $field)) {
-      $field['postal_code'] = (new self)
-          ->whitelist('A-Za-z0-9\\- ')
-          ->maxLength(10)
-          ->apply($field['postal_code']);
+		$temp = (new self);
+		$temp = $temp -> whitelist('A-Za-z0-9\\- ');
+		$temp = $temp -> maxLength(10);
+		$temp = $temp -> apply($field['postal_code']);;
+		$field['postal_code'] = $temp;
     }
     if (array_key_exists('phone', $field)) {
       static::fieldPhone($field['phone']);
@@ -95,15 +104,17 @@ class Veritrans_Sanitizer {
   private static function fieldPhone(&$field)
   {
     $plus = substr($field, 0, 1) === '+' ? true : false;
-    $field = (new self)
-        ->whitelist('\\d\\-\\(\\) ')
-        ->maxLength(19)
-        ->apply($field);
+	$temp = (new self);
+	$temp = $temp -> whitelist('\\d\\-\\(\\) ');
+	$temp = $temp -> maxLength(19);
+	$temp = $temp -> apply($field);
+    $field = $temp;
 
     if ($plus) $field = '+' . $field;
-    $field = (new self)
-        ->maxLength(19)
-        ->apply($field);
+    $temp = (new self);
+	$temp = $temp -> maxLength(19);
+	$temp = $temp -> apply($field);
+    $field = $temp;
   }
 
   private function maxLength($length)
